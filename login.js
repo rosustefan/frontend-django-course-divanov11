1d27f2e9b123ec744e186c8cdbb326246f21c3b9
Ștefan Roșu <44987333+rosustefan@users.noreply.github.com>
2022-11-10 16:46:54 +0200

    Add try/catch to login.js

--- login.js
+++ login.js
@@ -12,25 +12,30 @@ form.addEventListener('submit', (e) => {
     // console.log('Form Data:', formData)
 
     // 1. send the form data to this url
-    fetch('http://127.0.0.1:8000/api/users/token/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        // 2. pass the data into the body
-        body: JSON.stringify(formData)
-    })
-    // 3. and get the response
-    .then(response => response.json())
-    .then(data => {
-        // console.log('DATA:', data.access)
-        if(data.access) {
-            // set the received token in the browser's Local Storage
-            localStorage.setItem('token', data.access)
-            // and then redirect to the projects list
-            window.location = 'http://127.0.0.1:5500/projects-list.html'
-        } else {
-            alert('Username OR password did not work')
-        }
-    })
+    try {
+        fetch('http://127.0.0.1:8000/api/users/token/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            // 2. pass the data into the body
+            body: JSON.stringify(formData)
+        })
+        // 3. and get the response
+        .then(response => response.json())
+        .then(data => {
+            // console.log('DATA:', data.access)
+            if(data.access) {
+                // set the received token in the browser's Local Storage
+                localStorage.setItem('token', data.access)
+                // and then redirect to the projects list
+                window.location = 'http://127.0.0.1:5500/projects-list.html'
+            } else {
+                alert('Username OR password did not work')
+            }
+        })
+    }
+    catch(err) {
+        console.log(err)
+    }
 })
